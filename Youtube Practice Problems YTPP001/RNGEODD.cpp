96242b14b7ca55a480010443a1b6ec0207ddc889
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-06-19 12:49:00 +0530

    Create RNGEODD.cpp

---
+++ Youtube Practice Problems YTPP001/RNGEODD.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	
+}
+int main()
+{	speed_karo
+    int l,r;
+    cin>>l>>r;
+    for(int i=l;i<=r;i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+    return 0;
+}
