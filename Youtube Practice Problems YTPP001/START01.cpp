3c44926e8dc4212de0399885d89bb80eca037bfa
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-06-19 12:45:55 +0530

    Create START01.cpp

---
+++ Youtube Practice Problems YTPP001/START01.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	
+}
+int main()
+{	speed_karo
+    int t;
+    cin>>t;
+    cout<<t;
+    /*
+    while(t--)
+    {
+        solve();
+    }*/
+    return 0;
+}
