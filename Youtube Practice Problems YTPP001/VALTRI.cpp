85caa1a71480d6757a9bbe966adc50c7a571f64c
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-06-19 12:46:54 +0530

    Create VALTRI.cpp

---
+++ Youtube Practice Problems YTPP001/VALTRI.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	
+}
+int main()
+{	speed_karo
+    int n;
+    cin>>n;
+    if(n%5==0||n%6==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
