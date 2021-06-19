59221d8bb01a18aea1f2a57fb7d7ece22cce6903
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-06-19 12:48:18 +0530

    Create LSTSEV.cpp

---
+++ Youtube Practice Problems YTPP001/LSTSEV.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	
+}
+int main()
+{	speed_karo
+    int n;
+    cin>>n;
+    n=n/10;
+    if(n%10==7)
+    {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    return 0;
+}
