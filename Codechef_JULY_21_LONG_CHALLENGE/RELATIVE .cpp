a4e7d48c8cd6908e08c61f186e659737375deaa8
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-07-13 13:22:18 +0530

    Create RELATIVE .cpp

---
+++ Codechef_JULY_21_LONG_CHALLENGE/RELATIVE .cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	int g,c;
+ 	cin>>g>>c;
+ 	int h=(c*c)/(2*g);
+ 	cout<<h<<endl;
+}
+int main()
+{	speed_karo
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
