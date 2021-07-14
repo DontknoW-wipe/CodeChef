b5734a68debf88947f2ed46b6b77c290e815f298
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-07-14 15:07:11 +0530

    Create MUFFINS3.cpp

---
+++ Practices/MUFFINS3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+	ll n;
+	cin>>n;
+	cout<<n/2+1<<endl;
+}
+int main()
+{
+	speed_karo
+	int t;cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
