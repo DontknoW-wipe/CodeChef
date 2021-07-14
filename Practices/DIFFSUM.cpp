7a3b642a01dd0d704e5090a54c77911251ecf6d7
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-07-14 15:38:08 +0530

    Create DIFFSUM.cpp

---
+++ Practices/DIFFSUM.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void solve()
+{
+ 	
+}
+int main()
+{
+    int n1,n2;
+    cin>>n1;
+    cin>>n2;
+    if(n1>n2)
+    {
+        cout<<n1-n2<<endl;
+    }
+    else{
+        cout<<n1+n2<<endl;
+    }
+    return 0;
+}
